19f21ca11e1a3e1c08eab00d130e81a2ac1a9d71
wangdiam <wadisthis26@gmail.com>
2019-03-04 08:09:00 +0800

    Added reminder notifications

--- app/src/paid/java/com/wangdiam/studybuddycapstoneproject/application/StudyBuddyApp.java
+++ app/src/paid/java/com/wangdiam/studybuddycapstoneproject/application/StudyBuddyApp.java
@@ -3,6 +3,8 @@ package com.wangdiam.studybuddycapstoneproject.application;
 import com.google.firebase.database.FirebaseDatabase;
 
 public class StudyBuddyApp extends android.app.Application {
+    public static final String REMINDER_JOB_SERVICE = "REMINDER_JOB_SERVICE";
+
     @Override
     public void onCreate() {
         super.onCreate();
